f7fe31bb937799a48ea513ed88b63c5edc16e5c1
Aldo Oliveri <aolive52021@gmail.com>
2022-08-30 20:11:18 -0700

    Initial commit

---
+++ AD_Setup/Create-Users.ps1
@@ -0,0 +1,52 @@
+﻿# This script is intended to be run on an Active Directory Domain Controller and
+# will add 101 users from the employees.txt file.
+
+# The password needs to be converted to a secure string or we'll get an error message
+# Each user will have the generic password:  Pass1234
+# This can be changed at first logon, but for this lab, it is not necessary
+# Created by Aldo Oliveri - 8/25/2022
+
+$password = ConvertTo-SecureString "Pass1234" -AsPlainText -Force
+
+# This is the directory where employees.txt is located on the Domain Controller 
+$ADUsers = Get-Content C:\Users\Administrator\Documents\employees.txt
+
+
+# Create a for loop to create a username of format: first name initial and last name.
+
+foreach ($User in $ADUsers)
+{
+	$first_name = $User.Split(" ")[0].ToLower()
+	$last_name = $User.Split(" ")[1].ToLower()
+	$username = "$($first_name.Substring(0,1))$($last_name)".ToLower()
+
+	
+	# Check if the user account already exists in AD
+       
+	if (Get-ADUser -F {SamAccountName -eq $username})
+	{
+   		# If user does exist, output a warning message
+
+    	Write-Warning "Cannot create user account $username since it already exists."
+   	}
+       
+	else
+	{
+		# If a user does not exist then create a new user account
+       	# This next line will change the background and font color when output user created:
+
+    	Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+       
+		# This cmdlet will create the new user with the properties shown
+
+		New-AdUser -AccountPassword $password `
+		-GivenName $first_name `
+		-Surname $last_name `
+		-DisplayName $username `
+		-Name $username `
+		-EmployeeID $username `
+		-PasswordNeverExpires $true `
+		-Path "OU=Users-Test,DC=abc,DC=com" `
+		-Enabled $true
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Cybersecurity-Projects
+
+## Table of Contents
+
+[1.  Installing Active Directory and 100 Users in PowerShell](./AD_Setup/AD-Setup.md)
+
+[2.  In progress]()
